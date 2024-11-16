1d215014a22d8fae87c7bd3593e135dbd0685703
mmaksuti <103997986+mmaksuti@users.noreply.github.com>
2024-11-16 15:58:08 +0100

    Initialize client

---
+++ client/src/routes/AllRoutes.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const AllRoutes = () => {
+    return (
+        <Router>
+            <Routes>
+            </Routes>
+        </Router>
+    );
+}
+
+export default AllRoutes;
